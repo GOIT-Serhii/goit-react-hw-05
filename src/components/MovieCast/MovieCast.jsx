fcb51a7ef2b91cf79a683c32299d350d9d32e242
Serhii Bova <serschgold@gmail.com>
2024-09-21 16:12:58 +0300

    some corrections

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -6,7 +6,7 @@ import { getMovieCastById } from "../../api";
 import css from "./MovieCast.module.css";
 
 const defaultImg =
-  "<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>";
+  "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
 export default function MovieCast() {
   const { movieId } = useParams();
